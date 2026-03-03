aa00d402e5ed068da0875a76fa423540f4f2fb87
diaaicha <diaaicha3108@gmail.com>
2026-03-03 22:07:21 +0000

    Add Produit entity

---
+++ src/main/java/sn/edu/ugb/lumiere/entity/Produit.java
@@ -0,0 +1,28 @@
+package sn.edu.ugb.lumiere.entity;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "produits")
+public class Produit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nom;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal prix;
+
+    @Column(nullable = false)
+    private Integer quantite;
+
+    private String description;
+}

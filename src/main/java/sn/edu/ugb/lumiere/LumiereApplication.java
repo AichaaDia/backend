478bfaaab26a3eeb2aecf3af07f2de08f6fcc4f1
diaaicha <diaaicha3108@gmail.com>
2026-03-03 22:00:45 +0000

    Initial Spring Boot project from Spring Initializr

---
+++ src/main/java/sn/edu/ugb/lumiere/LumiereApplication.java
@@ -0,0 +1,13 @@
+package sn.edu.ugb.lumiere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumiereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumiereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/edu/ugb/lumiere/LumiereApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.edu.ugb.lumiere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumiereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
